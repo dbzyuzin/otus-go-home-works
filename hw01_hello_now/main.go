2078c4ddabcb24c28aec33837a867cfeefd94cef
d.zyuzin <d.zyuzin@spacemir.ru>
2020-03-28 15:41:32 +0300

    done

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"github.com/beevik/ntp"
+	"log"
+	"time"
+)
+
+const TimeSourceUrl = "0.beevik-ntp.pool.ntp.org"
+
 func main() {
-	// Place your code here
+	ntpTime, err := ntp.Time(TimeSourceUrl)
+
+	if err != nil {
+		log.Fatalf("ntp error: %s", err.Error())
+	}
+
+	fmt.Printf("current time: %s\n", formatTime(time.Now()))
+	fmt.Printf("exact time: %s\n", formatTime(ntpTime))
+}
+
+func formatTime(t time.Time) string {
+	return t.Format("2006-01-02 15:04:05 +0000 UTC")
 }
