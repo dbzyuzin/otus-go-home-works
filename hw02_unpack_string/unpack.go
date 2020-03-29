16f57371de6f6d3e06a2cdb6de8ebb99768840a5
d.zyuzin <d.zyuzin@spacemir.ru>
2020-03-29 21:43:01 +0300

    hw2 complete

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,33 @@ package hw02_unpack_string //nolint:golint,stylecheck
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here
-	return "", nil
+func Unpack(inp string) (string, error) {
+	inputReader := strings.NewReader(inp)
+
+	var result strings.Builder
+	for inputReader.Len() > 0 {
+		current, _, _ := inputReader.ReadRune()
+		next, _, _ := inputReader.ReadRune()
+
+		if unicode.IsDigit(current) {
+			return "", ErrInvalidString
+		}
+
+		if unicode.IsDigit(next) {
+			count, _ := strconv.Atoi(string(next))
+			result.WriteString(strings.Repeat(string(current), count))
+		} else {
+			_ = inputReader.UnreadRune()
+			result.WriteRune(current)
+		}
+	}
+
+	return result.String(), nil
 }
