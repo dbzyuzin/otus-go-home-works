aa78f8df05952befda832c92f5304e7988bc48f2
d.zyuzin <d.zyuzin@spacemir.ru>
2020-04-19 06:05:56 +0300

    done

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,27 @@
 package hw03_frequency_analysis //nolint:golint,stylecheck
+import (
+	"sort"
+	"strings"
+)
 
-func Top10(_ string) []string {
-	// Place your code here
-	return nil
+func Top10(inp string) []string {
+	words := strings.Fields(inp)
+
+	frequency := make(map[string]int)
+	for _, word := range words {
+		frequency[word]++
+	}
+
+	words = words[:0]
+	for word := range frequency {
+		words = append(words, word)
+	}
+	if len(words) <= 10 {
+		return words
+	}
+
+	sort.Slice(words, func(i, j int) bool {
+		return frequency[words[i]] > frequency[words[j]]
+	})
+	return words[:10]
 }
