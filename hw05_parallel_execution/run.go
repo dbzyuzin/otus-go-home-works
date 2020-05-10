8ffdf010d06829abdcec28e76285068c3cc8f814
d.zyuzin <d.zyuzin@spacemir.ru>
2020-05-10 23:23:28 +0300

    промежуточный

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -10,6 +10,68 @@ type Task func() error
 
 // Run starts tasks in N goroutines and stops its work when receiving M errors from tasks
 func Run(tasks []Task, N int, M int) error {
-	// Place your code here
+	results := make(chan error, 1)
+	tasksChan := make(chan Task, N)
+	quit := make(chan struct{})
+
+	var tnum int
+	for tnum = 0; tnum < N && tnum < len(tasks); tnum++ {
+		tasksChan <- tasks[tnum]
+	}
+	//go pushTasks(tasks, tasksChan, quit)
+
+	for i := 0; i < N; i++ {
+		go worker(tasksChan, results, quit)
+	}
+
+	errCounter := 0
+	for {
+		if <-results != nil {
+			errCounter++
+		}
+		if errCounter >= M {
+			close(tasksChan)
+			return ErrErrorsLimitExceeded
+		} else {
+			if tnum >= len(tasks) {
+				break
+			}
+			tasksChan <- tasks[tnum]
+			tnum++
+		}
+	}
+
 	return nil
+}
+func worker(in <-chan Task, out chan<- error, quit chan struct{}) {
+	for {
+		var task Task
+		select {
+		case <-quit:
+			return
+		case task = <-in:
+		}
+
+		if task == nil {
+			return
+		}
+		err := task()
+
+		select {
+		case <-quit:
+			return
+		case out <- err:
+		}
+	}
+}
+
+func pushTasks(tasks []Task, tasksChan chan<- Task, quit chan struct{}) {
+	defer close(tasksChan)
+	for _, task := range tasks {
+		select {
+		case <-quit:
+			return
+		case tasksChan <- task:
+		}
+	}
 }
