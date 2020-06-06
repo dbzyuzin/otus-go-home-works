56ed0e53abd00da94312b19517715661910c590e
d.zyuzin <d.zyuzin@spacemir.ru>
2020-06-06 12:21:28 +0300

    done

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"errors"
+	"github.com/mitchellh/ioprogress"
+	"io"
+	"os"
 )
 
 var (
@@ -9,7 +12,99 @@ var (
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
-func Copy(fromPath string, toPath string, offset, limit int64) error {
-	// Place your code here
+func Copy(fromPath string, toPath string, offset, limit int64) (err error) {
+	err = checkPaths(fromPath, toPath)
+	if err != nil {
+		return err
+	}
+
+	in, out, err, closeF := openFiles(fromPath, toPath)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		cerr := closeF()
+		if cerr != nil {
+			err = cerr
+		}
+	}()
+
+	stat, err := in.Stat()
+	if err != nil {
+		return err
+	}
+	if offset > stat.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+	if offset > 0 {
+		offset, err = in.Seek(offset, io.SeekStart)
+		if err != nil {
+			return err
+		}
+	}
+
+	inr := &ioprogress.Reader{
+		Reader: in,
+		Size:   stat.Size() - offset,
+	}
+
+	if limit > 0 {
+		_, err = io.CopyN(out, inr, limit)
+	} else {
+		_, err = io.Copy(out, inr)
+	}
+	if err != nil && err != io.EOF {
+		return err
+	}
+
+	err = out.Sync()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func openFiles(src, dst string) (in, out *os.File, err error, closer func() error) {
+	in, err = os.Open(src)
+	if err != nil {
+		return
+	}
+	out, err = os.Create(dst)
+	if err != nil {
+		return
+	}
+
+	return in, out, nil, func() error {
+		cerr := in.Close()
+		if cerr != nil {
+			return cerr
+		}
+		cerr = out.Close()
+		if cerr != nil {
+			return cerr
+		}
+		return nil
+	}
+}
+
+func checkPaths(src, dst string) (err error) {
+	sstat, err := os.Stat(src)
+	if err != nil {
+		return
+	}
+	if !sstat.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+	dstat, err := os.Stat(dst)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return
+		}
+	} else {
+		if !dstat.Mode().IsRegular() {
+			return ErrUnsupportedFile
+		}
+	}
+
 	return nil
 }
--- hw07_file_copying/go.mod
+++ hw07_file_copying/go.mod
@@ -1,3 +1,5 @@
-module github.com/fixme_my_friend/hw07_file_copying
+module github.com/dbzyuzin/otus-go-home-works/hw07_file_copying
 
 go 1.14
+
+require github.com/mitchellh/ioprogress v0.0.0-20180201004757-6a23b12fa88e
